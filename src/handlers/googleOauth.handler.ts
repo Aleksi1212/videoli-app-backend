a714ad39c2b9f74d36bad8a5c89d843295aa770b
Aleksi1212 <aleksinoro@gmail.com>
2023-08-04 13:26:15 +0300

    find or create user with google user data

--- src/handlers/googleOauth.handler.ts
+++ src/handlers/googleOauth.handler.ts
@@ -4,28 +4,40 @@ import {
     getGoogleOauthTokens,
     getGoogleUserData,
 } from '../services/googleOauth.service';
+import { findOrCreateUser } from '../services/user.service';
+import RouteError from '../utils/error.utils';
 
-async function googleOauthHandler(req: Request, res: Response, next: NextFunction) {
+async function googleOauthHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     const googleOauthCode = req.query.code as string;
+    let error: RouteErrorTypes;
 
     try {
-        const { id_token, access_token } = await getGoogleOauthTokens(googleOauthCode);
-        console.log({ id_token, access_token });
-    
+        const { id_token, access_token } = await getGoogleOauthTokens(
+            googleOauthCode
+        );
         const googleUser = await getGoogleUserData({ id_token, access_token });
-        console.log(googleUser);
-    
-        res.json({
-            tokens: {
-                id_token,
-                access_token
-            },
-            userData: {
-                googleUser
-            }
-        })
-    } catch(error: any) {
-        next(error)
+        if (!googleUser.verified_email) {
+            error = new RouteError('Google account not verified', 403);
+            return next(error);
+        }
+
+        const userData = await findOrCreateUser({
+            email: googleUser.email,
+            name: `${googleUser.given_name} ${googleUser.family_name}`,
+            profilePicture: googleUser.picture,
+        });
+        if (!userData) {
+            error = new RouteError('Error creating user', 500);
+            return next(error);
+        }
+
+        res.json(userData);
+    } catch (error: any) {
+        next(error);
     }
 }
 
